57a7cd7bdbf92136431389dd5506a7c4a455cbba
tacrande <alex.maldonado113@gmail.com>
2020-03-12 11:35:27 -0400

    Updating url

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/aalexmmaldonado/mbsgdml',
+    url='https://github.com/aalexmmaldonado/MB-sGDML',
     version='0.0.1',
     zip_safe=False,
 )
