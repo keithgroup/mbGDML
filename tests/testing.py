997aa4440fc87110b3b24d961f832d1da7bf73dc
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-16 09:27:19 -0400

    Updated tests

--- tests/testing.py
+++ tests/testing.py
@@ -1,7 +1,16 @@
 # Quick file to test parts of MB-sGDML as they are developed.
 import mbsgdml
 
+'''
 mbsgdml.train.prepare_gdml_files(
     '/home/alex/Dropbox/keith/projects/gdml/data/partitions/calculations/4acn/',
     '/home/alex/Dropbox/keith/projects/gdml/data/gdml-files/'
 )
+'''
+
+'''
+mbsgdml.train.prepare_gdml_dataset(
+    '/home/alex/Dropbox/keith/projects/gdml/data/gdml-files/5H2O',
+    '/home/alex/Dropbox/keith/projects/gdml/data/gdml-files/5H2O'
+)
+'''
