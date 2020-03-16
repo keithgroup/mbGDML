bd9470a7c970ca0735f3f517bf1391a525e92641
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-16 08:59:15 -0400

    Added quick testing file

---
+++ tests/testing.py
@@ -0,0 +1,7 @@
+# Quick file to test parts of MB-sGDML as they are developed.
+import mbsgdml
+
+mbsgdml.train.prepare_gdml_files(
+    '/home/alex/Dropbox/keith/projects/gdml/data/partitions/calculations/4acn/',
+    '/home/alex/Dropbox/keith/projects/gdml/data/gdml-files/'
+)
