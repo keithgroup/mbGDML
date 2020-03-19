898eef41c5aed1837656e29d35b393b90fa308bd
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-19 12:53:33 -0400

    Switch to pytest

--- tests/test_mbsgdml.py
+++ tests/test_mbsgdml.py
@@ -3,30 +3,21 @@
 """Tests for `mbgdml` package."""
 
 
-import unittest
+import pytest
 from click.testing import CliRunner
 
 from mbgdml import cli
 
 
-class TestMBGDML(unittest.TestCase):
-    """Tests for `mbgdml` package."""
+def test_000_something():
+    """Test something."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'mbgdml.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'mbgdml.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
