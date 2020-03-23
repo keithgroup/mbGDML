c00886d4512d08318e1af5beacecac41249e21ee
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-23 14:25:38 -0400

    Updated tests

--- tests/testing.py
+++ tests/testing.py
@@ -16,23 +16,24 @@ mbgdml.train.gdml_xyz_datasets(
 # script to convert xyz to npz
 # sgdml_dataset_from_extxyz.py
 
-
+'''
 test = mbgdml.train.MBGDMLTrain(
-    '/home/alex/Dropbox/keith/projects/mbgdml/data/gdml-datasets/H2O',
-    '/home/alex/Dropbox/keith/projects/mbgdml/data/gdml-models/H2O'
+    '/home/alex/Dropbox/keith/projects/mbgdml/data/gdml-datasets/MeOH',
+    '/home/alex/Dropbox/keith/projects/mbgdml/data/gdml-models/MeOH'
 )
 
-train_num = 500
-validate_num = 200
-test_num = 200
-sigma_range = '2:10:300'
+train_num = 100
+validate_num = 100
+test_num = 100
+sigma_range = '30:10:150'
 
 for dataset in test.dataset_paths:
     
-    if 'monomer' in dataset:
+    if 'tetramer' in dataset:
         test.train_GDML(
             dataset, train_num, validate_num, test_num, sigma_range
         )
-        
+'''       
+
 
 
