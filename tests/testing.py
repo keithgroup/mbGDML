0f377e2f76af592daeca4e55c8607b8367db9750
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-23 21:07:42 -0400

    Updated tests

--- tests/testing.py
+++ tests/testing.py
@@ -16,24 +16,24 @@ mbgdml.train.gdml_xyz_datasets(
 # script to convert xyz to npz
 # sgdml_dataset_from_extxyz.py
 
-'''
+
 test = mbgdml.train.MBGDMLTrain(
-    '/home/alex/Dropbox/keith/projects/mbgdml/data/gdml-datasets/MeOH',
-    '/home/alex/Dropbox/keith/projects/mbgdml/data/gdml-models/MeOH'
+    '/home/alex/Dropbox/keith/projects/mbgdml/data/quick-methanol/datasets',
+    '/home/alex/Dropbox/keith/projects/mbgdml/data/quick-methanol/models'
 )
 
 train_num = 100
 validate_num = 100
 test_num = 100
-sigma_range = '30:10:150'
+sigma_range = '2:10:100'  # Original is 2:10:100
 
 for dataset in test.dataset_paths:
     
-    if 'tetramer' in dataset:
+    if '3body' in dataset:
         test.train_GDML(
             dataset, train_num, validate_num, test_num, sigma_range
         )
-'''       
+      
 
 
 
