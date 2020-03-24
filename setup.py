60c76824e2a994b05193bbe2ae17afe18ec4bd7a
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-23 22:08:19 -0400

    Only Python 3.7

--- setup.py
+++ setup.py
@@ -20,17 +20,14 @@ test_requirements = ['pytest>=5.4.1', 'sgdml>=0.4.1']
 setup(
     author="Alex M. Maldonado",
     author_email='alex.maldonado113@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7'
     ],
     description="Many-body implementation of symmetric gradient domain machine learning",
     entry_points={
