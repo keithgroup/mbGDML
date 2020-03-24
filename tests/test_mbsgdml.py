6850f97880387f955f0b146b62288b813c638115
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-23 21:53:26 -0400

    Removed cli test

--- tests/test_mbsgdml.py
+++ tests/test_mbsgdml.py
@@ -4,20 +4,9 @@
 
 
 import pytest
-from click.testing import CliRunner
-
-from mbgdml import cli
+import mbgdml
 
 
 def test_000_something():
     """Test something."""
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'mbgdml.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
