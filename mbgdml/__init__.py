09a4b76575916639f29b4c19231f3652bd196d73
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-24 20:17:54 -0400

    Adding data module

--- mbgdml/__init__.py
+++ mbgdml/__init__.py
@@ -5,6 +5,7 @@ __email__ = 'alex.maldonado113@gmail.com'
 __version__ = '0.0.1'
 
 from mbgdml import calculate
+from mbgdml import data
 from mbgdml import generate
 from mbgdml import parse
 from mbgdml import partition
