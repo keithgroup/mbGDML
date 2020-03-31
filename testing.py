aa2c2ee563428e5040345c068e269a2afc853540
Alex Maldonado <alex.maldonado113@gmail.com>
2020-03-31 01:15:40 -0400

    Testing new dataset class

--- testing.py
+++ testing.py
@@ -1,13 +1,13 @@
 # Quick file to test parts of mbGDML as they are developed.
 import mbgdml
 
-'''
+
 mbgdml.data.create_datasets(
-    '/home/alex/Dropbox/keith/projects/mbgdml/data/partitions/calculations/4H2O/',
+    '/home/alex/Dropbox/keith/projects/mbgdml/data/partitions/calculations/5H2O/',
     '/home/alex/Dropbox/keith/projects/mbgdml/data/datasets/',
-    'bohr', 'hartree', theory='MP2/def2-TZVP'
+    'bohr', 'hartree', theory='MP2.def2-TZVP'
 )
-'''
+
 
 '''
 mbgdml.data.combine_datasets(
@@ -19,22 +19,18 @@ mbgdml.data.combine_datasets(
 
 '''
 test = mbgdml.train.MBGDMLTrain(
-    '/home/alex/Dropbox/keith/projects/mbgdml/data/quick-methanol/datasets',
-    '/home/alex/Dropbox/keith/projects/mbgdml/data/quick-methanol/models'
+    '/home/alex/Dropbox/keith/projects/mbgdml/data/datasets/H2O/4H2O-1mer-dataset.npz',
+    '/home/alex/Dropbox/keith/projects/mbgdml/data/models/H2O'
 )
 
-train_num = 100
-validate_num = 100
-test_num = 100
+train_num = 300
+validate_num = 300
+test_num = 300
 sigma_range = '2:10:100'  # Original is 2:10:100
-
-for dataset in test.dataset_paths:
-    
-    if '3body' in dataset:
-        test.train_GDML(
-            dataset, train_num, validate_num, test_num, sigma_range
-        )
-'''     
+test.train_GDML(
+    test.dataset, train_num, validate_num, test_num, sigma_range
+)
+ '''
 
 
 
