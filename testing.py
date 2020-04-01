817f4d58a298118291293f5a5ddf2a9bedc1b67f
Alex Maldonado <alex.maldonado113@gmail.com>
2020-04-01 18:27:37 -0400

    Updated tests

--- testing.py
+++ testing.py
@@ -39,20 +39,21 @@ dataset_path = f'{dataset_dir}{solvent_label}-{str(nbody)}mer-dataset.npz'
 models_dir = '/home/alex/Dropbox/keith/projects/mbgdml/data/models/'
 solvent_model_dir = f'{models_dir}{solvent_label}/'
 
-'''
+
 # Creates a new mbGDML dataset.
 dataset = mbgdml.data.mbGDMLDataset()
 dataset.load(dataset_path)
 dataset.mb_dataset(nbody, solvent_model_dir)
 dataset.save(dataset.base_vars['name'][()], dataset.base_vars, dataset_dir, True)
-'''
+
 
 # Training the new mbGDML model.
 mb_dataset_path = f'{dataset_dir}{solvent_label}-{nbody}body-dataset.npz'
 train_num = 300
 validate_num = 300
 test_num = 300
-sigma_range = '80:10:200'  # Original is 2:10:100
+sigma_range = '80:10:180'  # Original is 2:10:100
 test = mbgdml.train.MBGDMLTrain()
 test.load_dataset(mb_dataset_path)
 test.train_GDML(models_dir, train_num, validate_num, test_num, sigma_range)
+
