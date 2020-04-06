a0a05ecc327a6848ad0803c301bcb21fe72c76ca
Alex Maldonado <alex.maldonado113@gmail.com>
2020-04-06 19:37:51 -0400

    Added predictions that returns decomposed energy and forces

--- mbgdml/predict.py
+++ mbgdml/predict.py
@@ -38,45 +38,150 @@ class mbGDMLPredict():
         self.gdmls = mb_gdml
 
 
-    def predict(self, z, R):
+    def _calculate(self, z, R, system_size, molecule_size, gdml):
+        """The actual calculate/predict function for a single GDML model.
+
+        Predicts the energy and forces of a structure from a single GDML model.
+        In other words, it predicts the energy and force contribution from a
+        single many-body (or standard) GDML model.
         
+        Args:
+            z (list): Atomic numbers of all atoms in the system.
+            R (np.ndarray): Cartesian coordinates of all atoms of the structure
+                specified in the same order as z. The array should have shape
+                (n, 3) where n is the number of atoms.
+            system_size (int): Number of molecules of the defined chemical
+                species in the structure.
+            molecule_size (int): Number of atoms in the defined chemical
+                species.
+            gdml (sgdml.GDMLPredict): Object used to predict energies
+                and forces of the structure defined in R.
+        
+        Returns:
+            tuple: Dictionaries of contributions and total energies (float) and
+                forces (np.ndarray) of the structure. Note that forces has
+                the same shape as R. Each key of the dictionary is the molecule
+                combination. For example, '01' contains the predictions of
+                two-body energy and forces of the pair containing molecules 0
+                and 1. Additionally, each dict has 'T' representing the total
+                energy or forces.
+        """
+
+        # 'T' is for total
+        E_contributions = {'T': 0.0}
+        F_contributions = {'T': np.zeros(R.shape)}
+
+        nbody_order = int(gdml.n_atoms/molecule_size)
+        
+        # Getting list of n-body combinations (ints).
+        nbody_combinations = list(
+            itertools.combinations(list(range(0, system_size)), nbody_order)
+        )
+
+        # Getting all contributions for each molecule combination.
+        for comb in nbody_combinations:
+
+            # Gets indices of all atoms in the combination of molecules.
+            atoms = []
+            for molecule in comb:
+                atoms += list(range(
+                    molecule * molecule_size, (molecule + 1) * molecule_size
+                ))
+            
+            # Predicts energies
+            e, f = gdml.predict(R[atoms].flatten())
+
+            # Adds contributions prediced from model.
+            comb_label = ''.join(str(molecule) for molecule in comb)
+            E_contributions[comb_label] = e
+            F_contributions[comb_label] = f.reshape(len(atoms), 3)
+
+            # Adds contributions to total energy and forces.
+            E_contributions['T'] += E_contributions[comb_label]
+            F_contributions['T'][atoms] += F_contributions[comb_label]
+        
+        return E_contributions, F_contributions
+
+
+    def decomposed_predict(self, z, R):
+        """Computes predicted total energy and atomic forces decomposed by
+        many body order.
+        
+        Args:
+            z (list): Atomic numbers of all atoms in the system.
+            R (np.ndarray): Cartesian coordinates of all atoms of the structure
+                specified in the same order as z. The array should have shape
+                (n, 3) where n is the number of atoms.
+        
+        Returns:
+            tuple: Dictionaries of many-body contributions of total energies
+                (float) and forces (np.ndarray) of the structure. Note that
+                forces has the same shape as R. Each key of the dictionary is
+                the order of the many-body model. Within this dictionary is a
+                total, 'T', and molecule combinations identified in the system
+                and their contributions.
+        
+        Examples:
+        >>> atoms = [8, 1, 1, 8, 1, 1]
+        >>> coords = array([[ 1.530147,  1.121901,  0.396232],
+                            [ 2.350485,  1.1297  , -0.085235],
+                            [ 0.823842,  1.311077, -0.2846  ],
+                            [-1.464849, -1.155867, -0.449863],
+                            [-1.41447 , -1.99605 , -0.917981],
+                            [-0.868584, -1.275659,  0.32329 ]])
+        >>> E_predicted, F_predicted = decomposed_predict(atoms, coords)
+        >>> E_predicted['T']  # Units here are kcal/mol
+        -95775.24328734782
+        >>> F_predicted['T']  # Units here are kcal/(mol A)
+        array([[-4.06668790e+01,  5.97564871e+00, -1.63273803e+01],
+               [ 1.10787347e+01,  6.38884805e-01, -9.01790818e+00],
+               [ 2.82554119e+01, -8.11473944e+00,  2.53684238e+01],
+               [ 1.45845554e+01, -2.53514310e+00,  1.76065017e+01],
+               [-3.53967582e-02,  2.91676403e-01,  2.61011071e-01],
+               [-1.32164262e+01,  3.74366076e+00, -1.78906481e+01]])
+        """
+        # TODO run tests and add more information
+
         # Gets system information from dataset.
         # This assumes the system is only solvent.
         dataset_info = solvents.system_info(z.tolist())
         system_size = dataset_info['cluster_size']
         molecule_size = dataset_info['solvent_molec_size']
 
-        # Sets up arrays for storing predicted energies and forces.
-        F = np.zeros(R.shape)
-        E = 0.0
+        # 'T' is for total
+        E_contributions = {'T': 0.0}
+        F_contributions = {'T': np.zeros(R.shape)}
 
         # Adds contributions from all models.
         for gdml in self.gdmls:
             
-            model_atoms = int(gdml.n_atoms)
-            nbody_order = int(model_atoms/molecule_size)
-            
-            # Getting list of n-body combinations.
-            nbody_combinations = list(
-                itertools.combinations(list(range(0, system_size)), nbody_order)
-            )
+            nbody_order = int(gdml.n_atoms/molecule_size)
+            E_contributions[nbody_order], F_contributions[nbody_order] = \
+                self._calculate(z, R, system_size, molecule_size, gdml)
 
-            # Adding all contributions.
-            for comb in nbody_combinations:
-                # Gets indices of all atoms in the
-                # n-body combination of molecules.
-                atoms = []
-                for molecule in comb:
-                    atoms += list(range(
-                        molecule * molecule_size, (molecule + 1) * molecule_size
-                    ))
-                
-                # Adds n-body contributions prediced from nbody_model.
-                e, f = gdml.predict(R[atoms].flatten())
-                F[atoms] += f.reshape(len(atoms), 3)
-                E += e
+            # Adds contributions to total energy and forces.
+            E_contributions['T'] += E_contributions[nbody_order]['T']
+            F_contributions['T'] += F_contributions[nbody_order]['T']
         
-        return E, F
+        return E_contributions, F_contributions
+
+    def predict(self, z, R):
+        """Predicts total energy and atomic forces using many-body GDML models.
+        
+        Args:
+            z (list): Atomic numbers of all atoms in the system.
+            R (np.ndarray): Cartesian coordinates of all atoms of the structure
+                specified in the same order as z. The array should have shape
+                (n, 3) where n is the number of atoms.
+        
+        Returns:
+            tuple: Total energy of the system (float) and atomic forces
+                (np.ndarray) in the units specified by the GDML models.
+                Note that the forces have the same shape as R.
+        """
+        e, f = self.decomposed_predict(z, R)
+        
+        return e['T'], f['T']
 
 
 
@@ -126,16 +231,6 @@ class mbGDMLPredict():
         gdml = GDMLPredict(nbody_model)
         for config in range(num_config):
 
-            '''
-            # DEBUG START
-            if config > 380 and config < 385:
-                o_energy = base_vars['E'][config][()]
-                o_forces = base_vars['F'][config]
-                print(f'\n\n-----Configuration {config}-----')
-                print(f'Original Energy: {o_energy} kcal/mol')
-                print(f'Original Forces:\n{o_forces}')
-            # DEBUG END
-            '''
             for comb in nbody_combinations:
                 # Gets indices of all atoms in the
                 # n-body combination of molecules.
@@ -144,33 +239,13 @@ class mbGDMLPredict():
                     atoms += list(range(
                         molecule * molecule_size, (molecule + 1) * molecule_size
                     ))
-                '''
-                o_energy = E[config][()]
-                o_forces = F[config, atoms]
-                '''
+
                 # Removes n-body contributions prediced from nbody_model
                 # from the dataset.
                 e, f = gdml.predict(R[config, atoms].flatten())
                 F[config, atoms] -= f.reshape(len(atoms), 3)
                 E[config] -= e
 
-                '''
-                # DEBUG
-                if config > 380 and config < 385:
-                    print(f'\nMolecule combination: {comb}')
-                    print(f'Atoms: {atoms}')
-
-                    print(f'Original energy: {o_energy}')
-                    print(f'Predicted energy: {e[0]}')
-                    print(f'New energy: {E[config]}')
-
-                    print(f'Original forces:\n{o_forces}')
-                    print(f'Predicted forces:\n{f.reshape(len(atoms), 3)}')
-                    print(f'New forces:\n{F[config, atoms]}')
-                # DEBUG END
-                '''
-
-        
         # Updates dataset.
         base_vars['E'] = E
         base_vars['E_min'] = np.min(E.ravel())
