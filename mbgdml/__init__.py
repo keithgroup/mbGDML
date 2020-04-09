999e39e6ec5df3fd2e0e7d237f0fb19bcaedc1e3
Alex Maldonado <alex.maldonado113@gmail.com>
2020-04-09 19:37:27 -0400

    Adding analysis

--- mbgdml/__init__.py
+++ mbgdml/__init__.py
@@ -4,6 +4,7 @@ __author__ = """Alex M. Maldonado"""
 __email__ = 'alex.maldonado113@gmail.com'
 __version__ = '0.0.1'
 
+from mbgdml import analysis
 from mbgdml import calculate
 from mbgdml import data
 from mbgdml import generate
