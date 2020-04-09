eb8f9c7032e9de5c576a402ce6c3050b0255a0bd
Alex Maldonado <alex.maldonado113@gmail.com>
2020-04-09 11:22:13 -0400

    Preliminary force similarity

--- mbgdml/analysis.py
+++ mbgdml/analysis.py
@@ -21,11 +21,41 @@
 # SOFTWARE.
 
 """Analyses for mbGDML models."""
-
+import numpy as np
+from mbgdml.data import mbGDMLPredictset
 
 class NBodyContributions:
 
     def __init__(self):
         pass
+    
+    
+    def load_predictset(self, predictset_path):
+        self.predictset = mbGDMLPredictset()
+        self.predictset.read(predictset_path)
+
+    def force_similarity(self, predict_force, true_force):
+        """Compute modified cosine similarity of two force vectors.
+
+        Computes 1 - cosine_similarity. Two exact vectors will thus have a
+        similarity of 0, orthogal vectors will be 1, and equal but opposite
+        will be 2.
+        
+        Args:
+            predict_force (np.ndarray): Array of the predicted force vector by
+                GDML.
+            true_force (np.ndarray): Array of the true force vector of the same
+                shape as predict_force
+        
+        Returns:
+            float: [description]
+        """
+
+        similarity = np.dot(predict_force, true_force) / \
+                     (np.linalg.norm(predict_force) * \
+                      np.linalg.norm(true_force))
+
+        similarity = float(1 - similarity)
+        return similarity
 
     # TODO heat map that computes cosine similarity for 1-body up to n-body contributions of force vectors
