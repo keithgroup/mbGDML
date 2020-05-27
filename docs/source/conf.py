6d441304c6ab5ee965ef8d7582d192e73d673b63
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2020-05-26 21:38:13 -0400

    Fixing docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Updating master docs
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
