c086d8e28c8e207a891ea9229b27421cb95c143b
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2020-08-19 22:38:35 -0400

    Correcting name of repo directory in travis

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 
 # Fill this out with the correct org/repo
 ORG=keithgroup
-REPO=mbgdml
+REPO=mbGDML
 # This probably should match an email for one of your users.
 EMAIL=aalexmmaldonado@gmail.com
 
