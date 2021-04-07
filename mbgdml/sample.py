c25ce118b5bc8fc5788d882a63c306971a1d45da
Alex Maldonado <aalexmmaldonado@gmail.com>
2021-04-07 19:00:06 -0400

    Making sample criteria functions instead of class

--- mbgdml/sample.py
+++ mbgdml/sample.py
@@ -23,61 +23,53 @@
 import itertools
 import numpy as np
 
-class sampleCritera:
-    """A collection of structure selection criteria for data sets.
+def _calc_distance(r1, r2):
+    """Calculates the Euclidean distance between two points.
+
+    Parameters
+    ----------
+    r1 : :obj:`numpy.ndarray`
+        Cartesian coordinates of a point with shape ``(3,)``.
+    r2 : :obj:`numpy.ndarray`
+        Cartesian coordinates of a point with shape ``(3,)``.
     """
+    return np.linalg.norm(r1 - r2)
 
-    def __init__(self):
-        pass
+def distance(z, R, z_slice, cutoff):
+    """If the distance between all molecules is less than the cutoff.
 
-    def _calc_distance(self, r1, r2):
-        """Calculates the Euclidean distance between two points.
+    Distances larger than the cutoff will return ``False``, meaning they
+    should not be included in the data set.
 
-        Parameters
-        ----------
-        r1 : :obj:`numpy.ndarray`
-            Cartesian coordinates of a point with shape ``(3,)``.
-        r2 : :obj:`numpy.ndarray`
-            Cartesian coordinates of a point with shape ``(3,)``.
-        """
-        return np.linalg.norm(r1 - r2)
+    Parameters
+    ----------
+    z : :obj:`numpy.ndarray`
+        A ``(n,)`` shape array of type :obj:`numpy.int32` containing atomic
+        numbers of atoms in the structures in order as they appear.
+    R : :obj:`numpy.ndarray`
+        A :obj:`numpy.ndarray` with shape of ``(n, 3)`` where ``n`` is the
+        number of atoms with three Cartesian components.
+    z_slice : :obj:`numpy.ndarray`
+        Indices of the atoms to be used for the cutoff calculation.
+    cutoff : :obj:`list` [:obj:`float`]
+        Distance cutoff between the atoms selected by ``z_slice``. Must be
+        in the same units (e.g., Angstrom) as ``R``.
 
-    def distance(self, z, R, z_slice, cutoff):
-        """If the distance between all molecules is less than the cutoff.
+    Returns
+    -------
+    :obj:`bool`
+        If the distance between the two dimers is less than the cutoff.
+    """
+    if len(cutoff) != 1:
+        raise ValueError('Only one distance can be provided.')
 
-        Distances larger than the cutoff will return ``False``, meaning they
-        should not be included in the data set.
+    all_pairs = list(itertools.combinations(z_slice, 2))
 
-        Parameters
-        ----------
-        z : :obj:`numpy.ndarray`
-            A ``(n,)`` shape array of type :obj:`numpy.int32` containing atomic
-            numbers of atoms in the structures in order as they appear.
-        R : :obj:`numpy.ndarray`
-            A :obj:`numpy.ndarray` with shape of ``(n, 3)`` where ``n`` is the
-            number of atoms with three Cartesian components.
-        z_slice : :obj:`numpy.ndarray`
-            Indices of the atoms to be used for the cutoff calculation.
-        cutoff : :obj:`list` [:obj:`float`]
-            Distance cutoff between the atoms selected by ``z_slice``. Must be
-            in the same units (e.g., Angstrom) as ``R``.
-
-        Returns
-        -------
-        :obj:`bool`
-            If the distance between the two dimers is less than the cutoff.
-        """
-        if len(cutoff) != 1:
-            raise ValueError('Only one distance can be provided.')
-
-        all_pairs = list(itertools.combinations(z_slice, 2))
-
-        # Checks if any pair of molecules is farther away than the cutoff.
-        for pair in all_pairs:
-            distance = self._calc_distance(R[pair[0]], R[pair[1]])
-            if distance > cutoff[0]:
-                return False
-        # All pairs of molecules are within the cutoff.
-        return True
-        
+    # Checks if any pair of molecules is farther away than the cutoff.
+    for pair in all_pairs:
+        distance = _calc_distance(R[pair[0]], R[pair[1]])
+        if distance > cutoff[0]:
+            return False
+    # All pairs of molecules are within the cutoff.
+    return True
     
