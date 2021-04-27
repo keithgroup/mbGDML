f1180b0f35cc0970f3e49196d621d119e6ea0345
Alex Maldonado <aalexmmaldonado@gmail.com>
2021-04-27 09:19:27 -0400

    Reformatting analysis module

--- mbgdml/analysis/__init__.py
+++ mbgdml/analysis/__init__.py
@@ -2,6 +2,3 @@
 
 __author__ = """Alex M. Maldonado"""
 __email__ = 'aalexmmaldonado@gmail.com'
-
-from mbgdml.analysis.nbody import nbodyHeatMaps
-from mbgdml.analysis.structures import structureEmbedding
