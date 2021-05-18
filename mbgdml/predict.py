d8237dc2a2996dd03ab8df7e9ef8a384db6aa8eb
Alex Maldonado <aalexmmaldonado@gmail.com>
2021-05-18 19:02:20 -0400

    Updating predict methods to handle entity and comp ids

--- mbgdml/predict.py
+++ mbgdml/predict.py
@@ -24,7 +24,9 @@ import itertools
 
 import numpy as np
 from sgdml.predict import GDMLPredict
+from torch.functional import cartesian_prod
 import mbgdml.solvents as solvents
+from mbgdml import criteria
 
 class mbPredict():
     """Predict energies and forces of structures using GDML models.
@@ -51,34 +53,88 @@ class mbPredict():
         """
         self.gdmls = []
         self.models = []
+        self.entity_ids = []
+        self.comp_ids = []
+        self.criteria = []
+        self.z_slice = []
+        self.cutoff = []
         for model in models:
             loaded = np.load(model, allow_pickle=True)
-            self.models.append(loaded)
+            model = dict(loaded)
+            self.models.append(model)
             gdml = GDMLPredict(loaded)
             self.gdmls.append(gdml)
 
+            if model['criteria'] == '':
+                self.criteria.append(None)
+            else:
+                # Stores that actual criteria function from the criteria module.
+                self.criteria.append(getattr(criteria, str(model['criteria'])))
+            self.z_slice.append(model['z_slice'])
+            self.cutoff.append(model['cutoff'])
+            self.entity_ids.append(model['entity_ids'])
+            self.comp_ids.append(model['comp_ids'])
+    
+    def _generate_entity_combinations(self, r_entity_ids_per_model_entity):
+        """Generator for entity combinations where each entity comes from a
+        specified list.
+
+        Parameters
+        ----------
+        r_entity_ids_per_model_entity : :obj:`list` [:obj:`numpy.ndarray`]
+            A list of ``entity_ids`` that match the ``comp_id`` of each
+            model ``entity_id``. Note that the index of the
+            :obj:`numpy.ndarray``is equal to the model ``entity_id`` and the
+            values are ``r`` ``entity_ids`` that match the ``comp_id``.
+        """
+        nbody_combinations = itertools.product(*r_entity_ids_per_model_entity)
+        # Excludes combinations that have repeats (e.g., (0, 0) and (1, 1. 2)).
+        nbody_combinations = itertools.filterfalse(
+            lambda x: len(set(x)) <  len(x), nbody_combinations
+        )
+        # At this point, there are still duplicates in this iterator.
+        # For example, (0, 1) and (1, 0) are still included.
+        for combination in nbody_combinations:
+            if sorted(combination) == list(combination):
+                yield combination
 
 
-    def _calculate(self, R, system_size, molecule_size, gdml):
+    def _calculate(
+        self, r, entity_ids, comp_ids, model, gdml, ignore_criteria=False, 
+        store_each=True
+    ):
         """The actual calculate/predict function for a single GDML model.
 
         Predicts the energy and force contribution from a single many-body
-        (or standard) GDML model.
+        GDML model.
         
         Parameters
         ----------
-        R : :obj:`numpy.ndarray`
+        r : :obj:`numpy.ndarray`
             Cartesian coordinates of all atoms of the structure specified in 
             the same order as z. The array should have shape (n, 3) where n is 
-            the number of atoms.
-        system_size : :obj:`int`
-            Number of molecules of the defined chemical species in the 
-            structure.
-        molecule_size : :obj:`int`
-            Number of atoms in the defined chemical species.
+            the number of atoms. This is a single structure.
+        entity_ids : :obj:`numpy.ndarray`
+            An array specifying which atoms belong to what entities
+            (e.g., molecules) for ``r``.
+        comp_ids : :obj:`numpy.ndarray`
+            A 2D array relating ``entity_ids`` to a chemical component/species
+            id or label (``comp_id``) for ``r``. The first column is the unique
+            ``entity_id`` and the second is a unique ``comp_id`` for that
+            chemical species. Each ``comp_id`` is reused for the same chemical
+            species.
+        model : :obj:`dict`
+            The dictionary of the loaded npz file. Stored in ``self.models``.
         gdml : :obj:`sgdml.predict.GDMLPredict`
             Object used to predict energies and forces of the structure defined 
-            in ``R``.
+            in ``r``.
+        ignore_criteria : :obj:`bool`, optional
+            Whether to take into account structure criteria and their cutoffs
+            for each model. Defaults to ``False``.
+        store_each : :obj:`bool`, optional
+            Store each n-body combination's contribution in the :obj:`dict`.
+            Defaults to ``True``. Changing to ``False`` often speeds up
+            predictions.
         
         Returns
         -------
@@ -98,41 +154,64 @@ class mbPredict():
 
         # 'T' is for total
         E_contributions = {'T': 0.0}
-        F_contributions = {'T': np.zeros(R.shape)}
+        F_contributions = {'T': np.zeros(r.shape)}
 
-        nbody_order = int(gdml.n_atoms/molecule_size)
-        
-        # Getting list of n-body combinations (int).
-        nbody_combinations = list(
-            itertools.combinations(list(range(0, system_size)), nbody_order)
+        # Models have a specific entity order that needs to be conserved in the
+        # predictions. Here, we create a ``entity_idxs`` list where each item
+        # is a list of all entities in ``r`` that match the model entity.
+        r_entity_ids_per_model_entity = []
+        for model_comp_id in model['comp_ids']:
+            matching_entity_ids = np.where(comp_ids[:,1] == model_comp_id[1])[0]
+            r_entity_ids_per_model_entity.append(matching_entity_ids)
+
+        nbody_combinations = self._generate_entity_combinations(
+            r_entity_ids_per_model_entity
         )
 
-        # Getting all contributions for each molecule combination.
-        for comb in nbody_combinations:
+        # Getting all contributions for each molecule combination (comb).
+        for comb_entity_ids in nbody_combinations:
 
             # Gets indices of all atoms in the combination of molecules.
-            atoms = []
-            for molecule in comb:
-                atoms += list(range(
-                    molecule * molecule_size, (molecule + 1) * molecule_size
-                ))
+            # r_idx is a list of the atoms for the entity_id combination.
+            r_idx = []
+            for entity_id in comb_entity_ids:
+                r_idx.extend(np.where(entity_ids == entity_id)[0])
+            
+            # Checks criteria if present and desired.
+            if not ignore_criteria:
+                # If there is a cutoff specified.
+                if model['cutoff'].shape != (0,):
+                    r_criteria = getattr(criteria, str(model['criteria']))
+                    valid_r, _ = r_criteria(
+                        model['z'], r[r_idx], model['z_slice'],
+                        entity_ids[r_idx], cutoff=model['cutoff']
+                    )
+                    if not valid_r:
+                        # Do not include this contribution.
+                        continue
             
             # Predicts energies
-            e, f = gdml.predict(R[atoms].flatten())
+            e, f = gdml.predict(r[r_idx].flatten())
 
             # Adds contributions prediced from model.
-            comb_label = ','.join(str(molecule) for molecule in comb)
-            E_contributions[comb_label] = e
-            F_contributions[comb_label] = f.reshape(len(atoms), 3)
+            if store_each:
+                entity_label = ','.join(
+                    str(entity_id) for entity_id in comb_entity_ids
+                )
+                E_contributions[entity_label] = e
+                F_contributions[entity_label] = f.reshape(len(r_idx), 3)
 
             # Adds contributions to total energy and forces.
-            E_contributions['T'] += E_contributions[comb_label]
-            F_contributions['T'][atoms] += F_contributions[comb_label]
+            E_contributions['T'] += e
+            F_contributions['T'][r_idx] += f.reshape(len(r_idx), 3)
         
         return E_contributions, F_contributions
 
 
-    def decomposed_predict(self, z, R):
+    def decomposed_predict(
+        self, z, R, entity_ids, comp_ids, ignore_criteria=False,
+        store_each=True
+    ):
         """Computes predicted total energy and atomic forces decomposed by
         many-body order.
         
@@ -145,6 +224,22 @@ class mbPredict():
             A :obj:`numpy.ndarray` with shape of ``(m, n, 3)`` where ``m`` is
             the number of structures and ``n`` is the number of atoms with three 
             Cartesian components.
+        entity_ids : :obj:`numpy.ndarray`
+            An array specifying which atoms belong to what entities
+            (e.g., molecules).
+        comp_ids : :obj:`numpy.ndarray`
+            A 2D array relating ``entity_ids`` to a chemical component/species
+            id or label (``comp_id``). The first column is the unique
+            ``entity_id`` and the second is a unique ``comp_id`` for that
+            chemical species. Each ``comp_id`` is reused for the same chemical
+            species.
+        ignore_criteria : :obj:`bool`, optional
+            Whether to take into account structure criteria and their cutoffs
+            for each model. Defaults to ``False``.
+        store_each : :obj:`bool`, optional
+            Store each n-body combination's contribution in the :obj:`dict`.
+            Defaults to ``True``. Changing to ``False`` often speeds up
+            predictions.
         
         Returns
         -------
@@ -155,47 +250,27 @@ class mbPredict():
             the order of the many-body model. Within this dictionary is a
             total, 'T', and molecule combinations identified in the system
             and their contributions.
-        
-        Examples
-        --------
-        >>> atoms = [8, 1, 1, 8, 1, 1]
-        >>> coords = array([[ 1.530147,  1.121901,  0.396232],
-                            [ 2.350485,  1.1297  , -0.085235],
-                            [ 0.823842,  1.311077, -0.2846  ],
-                            [-1.464849, -1.155867, -0.449863],
-                            [-1.41447 , -1.99605 , -0.917981],
-                            [-0.868584, -1.275659,  0.32329 ]])
-        >>> E_predicted, F_predicted = decomposed_predict(atoms, coords)
-        >>> E_predicted['T']  # Units here are kcal/mol
-        -95775.24328734782
-        >>> F_predicted['T']  # Units here are kcal/(mol A)
-        array([[-4.06668790e+01,  5.97564871e+00, -1.63273803e+01],
-               [ 1.10787347e+01,  6.38884805e-01, -9.01790818e+00],
-               [ 2.82554119e+01, -8.11473944e+00,  2.53684238e+01],
-               [ 1.45845554e+01, -2.53514310e+00,  1.76065017e+01],
-               [-3.53967582e-02,  2.91676403e-01,  2.61011071e-01],
-               [-1.32164262e+01,  3.74366076e+00, -1.78906481e+01]])
         """
-        # TODO run tests and add more information
-
         # Gets system information from dataset.
         # This assumes the system is only solvent.
         if type(z) != list:
             z = z.tolist()
-        dataset_info = solvents.system_info(z)
-        system_size = dataset_info['cluster_size']
-        molecule_size = dataset_info['solvent_molec_size']
 
         # 'T' is for total
         E_contributions = {'T': 0.0}
         F_contributions = {'T': np.zeros(R.shape)}
 
         # Adds contributions from all models.
-        for gdml in self.gdmls:
-            
-            nbody_order = int(gdml.n_atoms/molecule_size)
+        for i in range(len(self.gdmls)):
+            gdml = self.gdmls[i]
+            model = self.models[i]
+            nbody_order = int(len(set(self.entity_ids[i])))
+
             E_contributions[nbody_order], F_contributions[nbody_order] = \
-                self._calculate(R, system_size, molecule_size, gdml)
+                self._calculate(
+                    R, entity_ids, comp_ids, model, gdml,
+                    ignore_criteria=ignore_criteria, store_each=store_each
+                )
 
             # Adds contributions to total energy and forces.
             E_contributions['T'] += E_contributions[nbody_order]['T']
@@ -204,7 +279,7 @@ class mbPredict():
         return E_contributions, F_contributions
 
 
-    def predict(self, z, R):
+    def predict(self, z, R, entity_ids, comp_ids, ignore_criteria=False):
         """Predicts total energy and atomic forces using many-body GDML models.
         
         Parameters
@@ -215,6 +290,18 @@ class mbPredict():
             Cartesian coordinates of all atoms of the structure specified in 
             the same order as ``z``. The array should have shape ``(n, 3)``
             where ``n`` is the number of atoms.
+        entity_ids : :obj:`numpy.ndarray`
+            An array specifying which atoms belong to what entities
+            (e.g., molecules).
+        comp_ids : :obj:`numpy.ndarray`
+            A 2D array relating ``entity_ids`` to a chemical component/species
+            id or label (``comp_id``). The first column is the unique
+            ``entity_id`` and the second is a unique ``comp_id`` for that
+            chemical species. Each ``comp_id`` is reused for the same chemical
+            species.
+        ignore_criteria : :obj:`bool`, optional
+            Whether to take into account structure criteria and their cutoffs
+            for each model. Defaults to ``False``.
         
         Returns
         -------
@@ -223,7 +310,10 @@ class mbPredict():
         :obj:`numpy.ndarray`
             Atomic forces of the system in the same shape as ``R``.
         """
-        e, f = self.decomposed_predict(z, R)
+        e, f = self.decomposed_predict(
+            z, R, entity_ids, comp_ids, ignore_criteria=ignore_criteria,
+            store_each=False
+        )
         
         return e['T'], f['T']
 
@@ -251,7 +341,10 @@ class mbPredict():
                 z_predict = z
             else:
                 z_predict = z[config]
-            e, f = self.predict(z_predict, R[config])
+            e, f = self.predict(
+                z_predict, R[config], nbody_dataset['entity_ids'],
+                nbody_dataset['comp_ids'], ignore_criteria=False
+            )
             F[config] -= f
             E[config] -= e
 
@@ -271,7 +364,6 @@ class mbPredict():
         # Tries to add model md5 hashes to data set
         mb_models_md5 = []
         for model in self.models:
-            model = dict(model)
             if 'md5' in model.keys():
                 mb_models_md5.append(model['md5'][()])
         nbody_dataset['mb_models_md5'] = mb_models_md5
