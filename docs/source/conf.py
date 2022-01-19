87293a80780751ed388a7a7e38c5ff246a4eedca
Alex Maldonado <aalexmmaldonado@gmail.com>
2022-01-18 21:41:50 -0500

    Bump year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'mbGDML'
-copyright = '2020-2021, Alex M. Maldonado'
+copyright = '2020-2022, Alex M. Maldonado'
 author = 'Alex M. Maldonado'
 
 
