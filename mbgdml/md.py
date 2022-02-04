0e84fb1ab437d1fada1c817f3155b8d44e1910bf
Alex Maldonado <aalexmmaldonado@gmail.com>
2022-02-04 16:30:03 -0500

    More descriptive ASE calculator class

--- mbgdml/md.py
+++ mbgdml/md.py
@@ -22,7 +22,7 @@
 
 import numpy as np
 from cclib.parser.utils import convertor
-from ase.calculators.calculator import Calculator
+from ase.calculators.calculator import Calculator as ASECalculator
 from ase.io import read
 from ase.optimize import QuasiNewton
 from ase.md.velocitydistribution import (MaxwellBoltzmannDistribution, Stationary, ZeroRotation)
@@ -33,8 +33,8 @@ from mbgdml.predict import mbPredict
 from mbgdml.data import mbGDMLData
 
 
-class mbGDMLCalculator(Calculator):
-    """Initializes mbGDML calculator with models and units.
+class mbGDML_ASE_Calculator(ASECalculator):
+    """Initializes mbGDML-ASE calculator with models and units.
     
     Parameters
     -----------
@@ -104,120 +104,3 @@ class mbGDMLCalculator(Calculator):
             f /= convertor(1, self.r_unit_model, 'Angstrom')
 
         self.results = {'energy': e, 'forces': f.reshape(-1, 3)}
-
-class mbGDMLMD(mbGDMLData):
-    """Molecular dynamics through ASE with many-body GDML models.
-        
-    Parameters
-    ----------
-    structure_name : :obj:`str`
-        Name of the structure. Mainly for file naming.
-    structure_path : :obj:`str`
-        Path to the structure file.
-    """
-
-    def __init__(self, structure_name, structure_path):
-        self._load_structure(structure_name, structure_path)
-
-    def _load_structure(self, structure_name, structure_path):
-        """Sets the appropriate attributes for structure information.
-        
-        Parameters
-        ----------
-        structure_name : :obj:`str`
-            Name of the structure. Mainly for file naming.
-        structure_path : :obj:`str`
-            Path to the structure file.
-        """
-        self.structure_name = structure_name
-        self.structure_path = structure_path
-        self.structure = read(structure_path) # mol
-        self.atoms = self.structure.numbers
-    
-    def load_calculator(self, model_paths):
-        """Loads the many-body GDML ASE calculator.
-        
-        Parameters
-        ----------
-        model_paths : :obj:`list`
-            Paths to all many-body GDML models to be used.
-        """
-
-        self.calc = mbGDMLCalculator(self.atoms, model_paths)
-        self.structure.set_calculator(self.calc)
-
-    def relax(self, max_force=1e-4, steps=100):
-        """Short relaxation of structure.
-        
-        Parameters
-        ----------
-        max_force : :obj:`float`, optional
-            Maximum force. Defaults to ``1e-4``.
-        steps : :obj:`int`, optional
-            Maximum allowable steps. Defaults to ``100``.
-        
-        Raises
-        ------
-        AttributeError
-            Requires a calculator to be loaded first.
-        """
-        if not hasattr(self, 'calc'):
-            raise AttributeError('Please load a calculator first.')
-
-        # do a quick geometry relaxation
-        qn = QuasiNewton(self.structure)
-        qn.run(max_force, steps)
-        
-    def printenergy(self, a):
-        """Quick function to print MD information during simulation.
-        
-        Parameters
-        ----------
-        a : :obj:`ase.atoms`
-            Atoms object from ASE.
-        """
-        # function to print the potential, kinetic and total energy
-        e_pot = a.get_potential_energy() / len(a)
-        e_kin = a.get_kinetic_energy() / len(a)
-        print('Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
-              'Etot = %.3feV' % (e_pot, e_kin, e_kin / (1.5 * units.kB),
-              e_pot + e_kin))
-    
-    def run(self, steps, t_step, temp):
-        """Runs a MD simulation using the Verlet algorithm in ASE.
-        
-        Parameters
-        ----------
-        steps : :obj:`int`
-            Number of steps for the MD simulation.
-        t_step : :obj:`float`
-            Time step in femtoseconds.
-        temp : :obj:`float`
-            Temperature in Kelvin used for initializing velocities.
-        
-        Raises
-        ------
-        AttributeError
-            Requires a calculator to be loaded first.
-        """
-        
-        if not hasattr(self, 'calc'):
-            raise AttributeError('Please load a calculator first.')
-
-        # Initialize momenta corresponding to temp
-        MaxwellBoltzmannDistribution(self.structure, temp * units.kB)
-        Stationary(self.structure) # zero linear momentum
-        ZeroRotation(self.structure) # zero angular momentum
-
-        self.name = f'{self.structure_name}-{t_step}fs-{steps}steps-{temp}K'
-        # run MD with constant energy using the VelocityVerlet algorithm
-        dyn = VelocityVerlet(
-            self.structure, t_step * units.fs,
-            trajectory=f'{self.name}.traj'
-        )
-
-        # now run the dynamics
-        self.printenergy(self.structure)
-        for i in range(steps):
-            dyn.run(10)
-            self.printenergy(self.structure)
